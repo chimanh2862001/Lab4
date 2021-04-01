8bbc57e40624ba5b61d31c2a6712de02ca50682c
nguyenchimanh <manhxinhtrai30@gmail.com>
2021-04-01 09:31:29 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var express=require('express');
+var app=express();
+var expressHBS=require('express-handlebars');
+app.engine('handlebars',expressHBS({
+   layoutsDir:__dirname+'/views/layouts',
+    defaultLayout:'main'
+}));
+
+app.set('view engine','handlebars');
+
+app.get('/',function (req,res){
+  // res.send('Hello World');
+  res.render('homepage');
+});
+
+
+app.post('/login',function (req,res){
+
+});
+
+app.listen(process.env.PORT||'3000');
